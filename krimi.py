f2f295353bd32324c99ca1ac04ca151721924aef
Krimi Patel <patelkrimi119@gmail.com>
2026-06-24 23:44:53 -0700

    py code

---
+++ krimi.py
@@ -0,0 +1,62 @@
+print("hello python")
+print("krimi patel")
+print("silver oak univerity")
+print("welcome to programming")
+print("i love python")
+print("Thursday")
+#print num
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+#fev subject
+print("python")
+print("learing is easy")
+print("ahmdabad")
+
+#variable task
+name="krimi"
+print(name)
+age=21
+print(age)
+phone_no = 9313449641
+print("my phone no is",phone_no)
+Email="patelkrimi6@gmail.com"
+print("my email adddres",Email)
+country="india"
+print("country name",country)
+message="welcome"
+print(message)
+city="ahmdabad"
+print(city)
+college_name="silver oak univercity"
+print(college_name)
+fev_color="black"
+print("my fev color",fev_color)
+fev_subject="python"
+print(fev_subject)
+
+
+
+
+
+name=input("krimi")
+print(name)
+age=int(input(21))
+print(age)
+phone_no = int(input(9313449641))
+print("my phone no is",phone_no)
+Email=input("patelkrimi6@gmail.com")
+print("my email adddres",Email)
+country=input("india")
+print("country name",country)
+message=input("welcome")
+print(message)
+city=input("ahmdabad")
+print(city)
+college_name=input("silver oak univercity")
+print(college_name)
+fev_color=input("black")
+print("my fev color",fev_color)
+fev_subject="python"
